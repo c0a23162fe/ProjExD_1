3c3096ae46fbc414e7b459a4aca0ddf00fbd56fc
yamada ayaka <c0a23162fe@edu.teu.ac.jp>
2024-04-16 17:55:53 +0900

    練習4

--- pygame_img.py
+++ pygame_img.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pygame as pg
 
+
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
@@ -9,16 +10,13 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     img = pg.image.load("fig/3.png")
     bird_img = pg.transform.flip(img, True, False)
-    tmr1 = 0
-    bird_rct = img.get_rect()
-    bird_rct.center = 300, 200
+    tmr = 0
+    clock.tick(10)
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        screen.blit(bg_img, [tmr1, 0])
-        screen.blit(bird_img, bird_rct)
-        clock.tick(200)
-
+        screen.blit(bg_img, [tmr, 0])
+        screen.blit(bird_img, 300, 200)
 
 if __name__ == "__main__":
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
